f350ee0e796da6ee8b4176f74b3203e5dc80eda2
tyler <tylercobb92@gmail.com>
2022-09-11 23:46:55 -0400

    wrote test for /users route

---
+++ tests/test_flask.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+
+from app import app
+from models import db, User
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+db.drop_all()
+db.create_all()
+
+class UserViewsTestCase(TestCase):
+    def setUp(self):
+        User.query.delete()
+
+        user = User(first_name='John', last_name='Smith')
+        db.session.add(user)
+        db.session.commit()
+        self.user_id=user.id
+
+    def tearDown(self):
+        db.session.rollback()
+
+    def test_list_users(self):
+        with app.test_client() as client:
+            resp = client.get('/users')
+            html = resp.get_data(as_text=True)
+
+            self.assertIn('John', html)
+            self.assertIn('Smith', html)
