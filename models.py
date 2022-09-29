be6eedb023482b6e4a02a7ee181097ad3a373ca1
tyler <tylercobb92@gmail.com>
2022-09-29 14:40:54 -0400

    added Post class

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -17,9 +18,20 @@ class User(db.Model):
         return f"{self.first_name} {self.last_name}"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
     first_name=db.Column(db.String, nullable=False)
-    
     last_name=db.Column(db.String, nullable=False)
-
     image_url=db.Column(db.String, nullable=False, default=default_img)
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    # @property from solution: lookup how it works
+    # @property
+    # def friendly_date(self):
+    #     return self.created_at.strftime("%a %b %-d %Y, %-I:%M %p")
