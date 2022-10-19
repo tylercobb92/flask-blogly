2783f0be41d41856683b56f7e16b866f13f455b0
tyler <tylercobb92@gmail.com>
2022-10-19 14:17:55 -0400

    added tags models and relationships to existing models

--- models.py
+++ models.py
@@ -37,3 +37,17 @@ class Post(db.Model):
     def friendly_date(self):
         # %a = weekday, %b = month, %-d = numeral for day without leading 0, %Y = year, %-I numeral hour without leading 0, %M = numeral minutes, %p = AM/PM
         return self.created_at.strftime("%a %b %-d %Y, %-I:%M %p")
+
+class PostTag(db.Model):
+    __tablename__='posts_tags'
+
+    post_id=db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id=db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+class Tag(db.Model):
+    __tablename__='tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    posts = db.relationship('Post', secondary='posts_tags', backref='tags')
